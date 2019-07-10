3517f16a10b67c4752eb05ae3fe7cdfbfaf1d6e4
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-10 09:52:15 +0000

    Done.

--- index.js
+++ index.js
@@ -11,25 +11,38 @@
 // }
 
 
+// function findMatching(drivers,name){
+//   return drivers.filter(function(driver){
+//   return (driver.toLowerCase() === name.toLowerCase()) ;
+// });
+// }
+
+// function fuzzyMatch(drivers, str){
+//   return drivers.filter(function (driver){
+//     return driver.indexOf(str) === 0;
+//   })
+// }
+
+// function matchName(drivers, matchStr){
+//   const matchArr = [];
+//   for (let driver of drivers){
+//     if (driver['name'] === matchStr){
+//       matchArr.push(driver);  
+      
+//     }
+//   }
+//   return matchArr;
+// }
+
 function findMatching(drivers,name){
-  return drivers.filter(function(driver){
-  return (driver.toLowerCase() === name.toLowerCase()) ;
-});
+  return drivers.filter(driver => driver.toLowerCase() === name.toLowerCase());
+
 }
 
 function fuzzyMatch(drivers, str){
-  return drivers.filter(function (driver){
-    return driver.indexOf(str) === 0;
-  })
+  return drivers.filter(driver => driver.indexOf(str) === 0);
 }
 
-function matchName(drivers, matchStr){
-  const matchArr = [];
-  for (let driver of drivers){
-    if (driver['name'] === matchStr){
-      matchArr.push(driver);  
-      
-    }
-  }
-  return matchArr;
+function matchName(drivers , matchStr){
+  return drivers.filter(driver => driver.name === matchStr);
 }
