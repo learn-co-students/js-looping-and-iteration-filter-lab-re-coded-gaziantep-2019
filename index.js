fab9963d1870730c4961ee6497107407f482868e
Abdelnasser Alkhellow <abdelnasser.kh10@gmail.com>
2019-07-10 09:35:01 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file
+
+// function findMatching(drivers,name){
+//   const result = [];
+//   for(const driver of drivers){
+//     if (name.toLowerCase() === driver.toLowerCase()){
+//       result.push(driver);
+//   }
+// }
+//   return result;
+// }
+
+
+function findMatching(drivers,name){
+  return drivers.filter(function(driver){
+  return (driver.toLowerCase() === name.toLowerCase()) ;
+});
+}
+
+function fuzzyMatch(drivers, str){
+  return drivers.filter(function (driver){
+    return driver.indexOf(str) === 0;
+  })
+}
+
+function matchName(drivers, matchStr){
+  const matchArr = [];
+  for (let driver of drivers){
+    if (driver['name'] === matchStr){
+      matchArr.push(driver);  
+      
+    }
+  }
+  return matchArr;
+}
