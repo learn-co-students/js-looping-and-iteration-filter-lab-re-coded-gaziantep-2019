bae9465e1d7ff834a6a7122e076eb2f9986198ee
Ammar Sammour <ammar.n.sammour@gmail.com>
2019-07-16 14:03:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Code your solution in this file
+function findMatching(drivers, match){
+  return drivers.filter(driver => driver.toLowerCase() === match.toLowerCase())
+}
+
+function fuzzyMatch(drivers, match){
+  return drivers.filter(driver => driver.slice(0,2).includes(match))
+}
+function matchName(drivers, match){
+  return drivers.filter(driver => driver.name === match)
+}
