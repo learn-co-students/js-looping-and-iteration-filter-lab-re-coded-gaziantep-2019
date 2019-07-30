fe84e375c6b4b5a916d8c72a9c23163abddf0e6a
Maya Nour Alah <maya.noorallah.89@gmail.com>
2019-07-30 13:38:41 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file
+function findMatching(drivers,name){
+  const matchingDrivers = drivers.filter(function (driver) {
+    return driver.toLowerCase() === name.toLowerCase(); });
+ 
+ return matchingDrivers;
+}
+
+function fuzzyMatch(drivers, subString){
+  const matchingDrivers = drivers.filter(function (driver) {
+    return driver.startsWith(subString); });
+ 
+ return matchingDrivers; 
+  
+}
+
+function matchName(drivers, name){
+  const matchingDrivers = drivers.filter(function (driver) {
+    return driver.name === name });
+ 
+ return matchingDrivers;
+}
